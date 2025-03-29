0b3037e6f66eae9bb0142876e2e5fc8c745743c7
Legatoxm01 <saap2694@gmail.com>
2025-03-29 14:41:01 -0500

    se construye un condicional compuesto para validar una mayoria de edad

--- Condicionales/src/App.java
+++ Condicionales/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int edad;
+        System.out.println("ingrese su edad");
+        edad = sc.nextInt();
+        if (edad > 18){
+            System.out.println("eres mayor de edad");
+        } else{
+            System.out.println("Usted aun esta muy pollo");
+        }
+        sc.close();
     }
 }
